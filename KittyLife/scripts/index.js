b6f4ff0be87912569c3f229cbfd5b10ec093f7b7
MagicalDaniel1 <146736354+MagicalDaniel1@users.noreply.github.com>
2025-02-10 16:11:51 +0200

    Update index.js

--- KittyLife/scripts/index.js
+++ KittyLife/scripts/index.js
@@ -1,32 +1,38 @@
-let daytasks = ["8:30-13:40 Пережить пары","14:00-16:00 Свободное время","16:00-18:00 Курсы (Различные)","18:00-19:00 Выполнение Д/з (Колледж)","20:00-22:30 Работа над своими проектами (Или над Д/з по программированию)"];
-        function kitty_emotions() {
-        let energy_bar = 184 - daytasks.length * 18.4;
-        if (energy_bar >= 92) {
-        document.getElementById("kitty").src = "./images/Kitty.gif";
-    }
-        if (energy_bar >= 150) {
-        document.getElementById("kitty").src = "./images/Happy_Kitty.gif";
-    }
-        if (energy_bar <= 91) {
-        document.getElementById("kitty").src = "./images/Scared_Kitty.gif";
-    }
-        if (energy_bar <= 45) {
-        document.getElementById("kitty").src = "./images/Sad_Kitty.gif";
-    };
-        if (energy_bar <= 0) {
-        document.getElementById("kitty").src = "./images/Dead_Kitty.png";
-        alert("Вы слишком сильно нагрузили котейку работой. К сожалению для вас, вам придётся облегчить его расписание");
-    };
-};
-        window.onload = function()
+window.onload = function()
 {
     const fade = document.getElementById('body');
     fade.classList.add('v');
 }
 
-let energy_bar = 184 - 'daytasks.length' * 18.4;
-if (energy_bar <= 80) {
-    document.getElementById("kitty").src = "/images/Shy_Kitty.gif";
+let daytasks = [
+    "8:30-13:40 Пережить пары",
+    "14:00-16:00 Свободное время",
+    "16:00-18:00 Курсы (Различные)",
+    "18:00-19:00 Выполнение Д/з (Колледж)",
+    "20:00-22:30 Работа над своими проектами (Или над Д/з по программированию)"
+];
+
+function add_ex() {
+    let taskInput = document.getElementById("add_ex_field").value;
+
+    if (taskInput === "") {
+        document.getElementById('add_ex_field').value = "Внесите задачу!";
+        return;
+    }
+
+    daytasks.push(taskInput);
+    document.getElementById("add_ex_field").value = "";
+
+    show_work_list();
+    bar_scale();
+    kitty_emotions();
+}
+
+function delete_ex(num) {
+    daytasks.splice(num, 1);
+    show_work_list();
+    bar_scale();
+    kitty_emotions();
 }
 
 function show_work_list() {
@@ -47,12 +53,7 @@ function show_work_list() {
     }
 }
 
-function delete_ex(num) {
-    daytasks.splice(num, 1);
-    show_work_list();
-    bar_scale();
-    kitty_emotions();
-}
+let energy_bar = 184 - 'daytasks.length' * 18.4;
 
 function bar_scale() {
     document.getElementById("energy_bar_size").style.width = 184 - daytasks.length * 18.4 + "px";
@@ -60,18 +61,22 @@ function bar_scale() {
 }
 bar_scale();
 
-function add_ex() {
-    let taskInput = document.getElementById("add_ex_field").value;
-
-    if (taskInput === "") {
-        document.getElementById('add_ex_field').value = "Внесите задачу!";
-        return;
+function kitty_emotions() {
+    let energy_bar = 184 - daytasks.length * 18.4;
+    if (energy_bar >= 92) {
+        document.getElementById("kitty").src = "./images/Kitty.gif";
     }
-
-    daytasks.push(taskInput);
-    document.getElementById("add_ex_field").value = "";
-
-    show_work_list();
-    bar_scale();
-    kitty_emotions();
-}
+    if (energy_bar >= 150) {
+        document.getElementById("kitty").src = "./images/Happy_Kitty.gif";
+    }
+    if (energy_bar <= 91) {
+        document.getElementById("kitty").src = "./images/Scared_Kitty.gif";
+    }
+    if (energy_bar <= 45) {
+        document.getElementById("kitty").src = "./images/Sad_Kitty.gif";
+    };
+    if (energy_bar <= 0) {
+        document.getElementById("kitty").src = "./images/Dead_Kitty.png";
+        alert("Вы слишком сильно нагрузили котейку работой. К сожалению для вас, вам придётся облегчить его расписание");
+    };
+};
